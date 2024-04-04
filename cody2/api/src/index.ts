72eb5d5328c6779fcf3e1ddb89991c4d20088c77
Samanimkr <Samanimkr@outlook.com>
2024-04-03 23:00:11 -0400

    socket io setup

--- cody2/api/src/index.ts
+++ cody2/api/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import http from "http";
+import cors from "cors";
 import { Server } from "socket.io";
 
 const app = express();
+app.use(cors());
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } });
 
 //an event listener is set up for new WebSocket connections and passes a socket object that represents the connection.
 io.on("connection", (socket) => {
--- cody2/frontend/src/index.tsx
+++ cody2/frontend/src/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
+import { SocketProvider } from './contexts/SocketContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <SocketProvider>
+      <App />
+    </SocketProvider>
   </React.StrictMode>
 );
